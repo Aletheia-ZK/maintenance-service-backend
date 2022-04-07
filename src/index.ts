211d716f774d5ce63e0a16ac5176ae3852b3a12b
Jan Ruettinger <service+github@janruettinger.com>
2022-04-06 22:18:33 -0700

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import { getReputationHolders } from './reputations/reputation_1'
 
 async function main() {
     try {
+        // get pub addresses of everybody who fulfills reputation 1
         const reputation1Holders = await getReputationHolders()
+
+        // create merkle tree with poseidon hash function
         console.log(reputation1Holders)
 
     } catch (error) {
