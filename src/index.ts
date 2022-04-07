84b1bdd3ff389ac66fd0df108e66da1b3ed6d3f1
Jan Ruettinger <service+github@janruettinger.com>
2022-04-07 15:08:57 -0700

    run reputation check every 10 seconds

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,24 @@ import { getReputationHolders } from './reputations/reputation_1'
 import { buildMerkleTree } from './tree_helper_functions'
 
 
-async function main() {
+async function maintainReputation1(){
+    console.log("Check for updates on reputation 1 merkle tree...")
     try {
         // get pub addresses of everybody who fulfills reputation 1
         const reputation1Holders = await getReputationHolders()
-        console.log(reputation1Holders)
+        console.log("Number of reputation 1 users: ", reputation1Holders.length)
 
         // create merkle tree with poseidon hash function
         const tree = buildMerkleTree(reputation1Holders)
-        console.log("root:",tree.root)
-        console.log("Tree:", tree.toString())
-
+        console.log("New root:",tree.root)
+        // console.log("Tree:", tree.toString())
     } catch (error) {
         console.log(error)
     }
+}
+
+async function main() {
+    setInterval(maintainReputation1, 10000);
 }
 
 main()
