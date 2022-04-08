8ddc0c04facf099b6ab58a205ed97eab84e559ac
Jan Ruettinger <service+github@janruettinger.com>
2022-04-07 23:39:24 -0700

    add local network to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,18 +10,18 @@ require("@nomiclabs/hardhat-ethers")
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Be aware of NEVER putting real Ether into testing accounts
-const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 module.exports = {
     solidity: '0.8.0',
     networks: {
         testnet: {
             url: `https://api.s0.b.hmny.io`,
-            accounts: [`0x${WALLET_PRIVATE_KEY}`],
+            accounts: [`0x${PRIVATE_KEY}`],
         },
         mainnet: {
             url: `https://api.harmony.one`,
-            accounts: [`0x${WALLET_PRIVATE_KEY}`],
+            accounts: [`0x${PRIVATE_KEY}`],
         },
         localhost: {
             url: "http://127.0.0.1:8545",
