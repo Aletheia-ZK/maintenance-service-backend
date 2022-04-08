51ed3c378cde99f8881113add86d77043bd9dc24
Jan Ruettinger <service+github@janruettinger.com>
2022-04-07 20:44:32 -0700

    add first draft of contract to store merkle tree roots

---
+++ contracts/Athletia.sol
@@ -0,0 +1,16 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.10;
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract Athletia is Ownable {
+    // store merkle tree roots for different types of reputations
+    mapping(string => bytes32) public roots;
+
+    function getRoot(string calldata name) public view returns (bytes32) {
+        return roots[name];
+    }
+
+    function setRoot(string calldata name, bytes32 value) public onlyOwner {
+        roots[name] = value;
+    }
+}
