bb020bb7cf2a727bae090865d98bb0739e5b3bb8
Jan Ruettinger <service+github@janruettinger.com>
2022-04-09 21:22:54 -0700

    store root and leaves in redis

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
 import { getReputationHolders } from './reputations/reputation1'
 import { buildMerkleTree, updateRoot } from './treeHelperFunctions'
+import { createClient } from 'redis'
 
-async function maintainReputation1(){
-    console.log("Check for updates on reputation 1 merkle tree...")
+async function maintainReputation1(client: any) {
+    console.log('Check for updates on reputation 1 merkle tree...')
     try {
         // get pub addresses of everybody who fulfills reputation 1
         const reputation1Holders = await getReputationHolders()
-        console.log("Number of reputation 1 users: ", reputation1Holders.length)
+        console.log('Number of reputation 1 users: ', reputation1Holders.length)
 
         // create merkle tree with poseidon hash function
         const tree = buildMerkleTree(reputation1Holders)
-        console.log("New root:",tree.root)
+        console.log('New root:', tree.root)
 
-        // update root of contract
-        console.log("update root in contract")
-        updateRoot("reputation_1", tree.root.toString())
+        // update root in smart contract
+        console.log('Update root in contract')
+        // updateRoot('reputation_1', tree.root.toString())
 
-        // read root from contract
-        // console.log("read root for reputation1 from contract")
-        // getRoot("reputation_1")
-
-        // update merkle root in smart contract
+        // update values in redis
+        console.log('Update values in Redis')
+        await client.set('attestation_1_leaves', JSON.stringify(tree.elements))
+        await client.set('attestation_1_root', tree.root)
     } catch (error) {
         console.log(error)
     }
 }
 
 async function main() {
-    setInterval(maintainReputation1, 10000);
+    const client = createClient()
+    client.on('error', (err) => console.log('Redis Client Error', err))
+    await client.connect()
+    setInterval(() => {
+        maintainReputation1(client)
+    }, 10000)
 }
 
 main()
